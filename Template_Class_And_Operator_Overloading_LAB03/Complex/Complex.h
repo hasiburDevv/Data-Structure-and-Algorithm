4a51c9c9165f6f2031ec7453c0981c9a4188d970
linux <shanto.ewu99@gmail.com>
2021-03-19 05:25:03 +0600

    added Complex project in LAB03

--- Template_Class_And_Operator_Overloading_LAB03/Complex/Complex.h
+++ Template_Class_And_Operator_Overloading_LAB03/Complex/Complex.h
@@ -7,7 +7,8 @@ private:
 public:
     Complex(); // constructor with no argument
     Complex(double, double); // constructor with two argument
-    Complex operator+ (Complex); // overloading "+" operator
+
+    Complex operator+ (Complex); // Complex --> return type, operator+ --> function name, (Complex) --> argument as a Complex
     Complex operator* (Complex); // overloading "*" operator
     bool operator!= (Complex); // overloading "!=" operator
     void Print();
